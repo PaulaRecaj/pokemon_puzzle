9caa68586d04adc66c3c1052221796c1430f24a5
PaulaRecaj <131966389+PaulaRecaj@users.noreply.github.com>
2024-10-08 14:22:09 +0000

    puzzle

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,7 +1,15 @@
 class Pokemon {
 
     // (1)
+    static activePokemon = null;
     // (2)
+    static keys = {
+      ArrowUp: false,
+      ArrowDown: false,
+      ArrowLeft: false,
+      ArrowRight: false
+  };
+
 
     constructor(name, sprite) {
         this.name = name;
@@ -12,34 +20,54 @@ class Pokemon {
     
     createElement() {
       // (3) 
+      const img = document.createElement('img');
+      img.src = this.sprite;
+      img.style.position = 'absolute';
+      img.style.top = Math.ceil(Math.random()*100) + 'px';
+      img.style.left = Math.ceil(Math.random()*100) + 'px';
+      document.body.appendChild(img);
+      return img;
     }
     
     addEventListeners() {   
      	// (4)
+      his.element.addEventListener('click', () => {
+        Pokemon.activePokemon = this;
+      });
     }
     
     move(step) { 
     	// (5)
+      let top = parseInt(this.element.style.top);
+      let left = parseInt(this.element.style.left);
+      if (Pokemon.keys.ArrowUp) 
+        this.element.style.top = (top + step) + 'px';
+      if (Pokemon.keys.ArrowDown)    
+        this.element.style.top = (top - step) + 'px';
+      if (Pokemon.keys.ArrowLeft) 
+        this.element.style.left = (left + step) + 'px';
+      if (Pokemon.keys.ArrowRight) 
+        this.element.style.left = (left + step) + 'px';
     }
 } // end of Pokemon class
 
 
 document.addEventListener('keydown', function (event) {
-  
-   // (6)
-
+  // (6)
+  Pokemon.keys[event.key] = true;
 });
 
 document.addEventListener('keyup', function (event) {
-   
   // (7)
-
+  Pokemon.keys[event.key] = false;
 });
 
 function moveActivePokemon() {
-   
   // (8) 
-
+  const step = 5;
+    if (Pokemon.activePokemon) {
+        Pokemon.activePokemon.move(step);
+    }
 }
 
 setInterval(moveActivePokemon, 10);
@@ -49,4 +77,9 @@ const pokemonNames = ['pikachu', 'bulbasaur', 'charmander', 'squirtle', 'jigglyp
 
 pokemonNames.forEach(name => {
     //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(r => r.json())
+        .then(data => {
+            new Pokemon(name, data.sprites.front_shiny);
+        });
 });
